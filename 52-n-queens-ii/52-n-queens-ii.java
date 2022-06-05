1db2556aba0c2fbca466e0ea4de2342b38b16fd8
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2022-06-05 12:06:08 +0530

    Time: 1 ms (97.89%), Space: 40.9 MB (59.37%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,19 +1,16 @@
 class Solution {
-    int sol = 0;
     public int totalNQueens(int n) {
         int[] before = new int[n];
         int[] upDi   = new int[n * 2 - 1];
         int[] loDi   = new int[n * 2 - 1];
         
-        f(before, upDi, loDi, 0, n);
-        
-        return sol;
+        return f(before, upDi, loDi, 0, n, 0);
     }
     
-    private void f(int[] before, int[] upDi, int[] loDi, int col, int n) {
+    private int f(int[] before, int[] upDi, int[] loDi, int col, int n, int count) {
         if(col == n) {
-            sol = sol + 1;
-            return;
+            count = count + 1;
+            return count;
         }
         
         for(int row = 0; row < n; row++) {
@@ -22,18 +19,18 @@ class Solution {
                 upDi[row + col] == 0 &&
                 loDi[(n-1) + (col - row)] == 0
             ) {
-                // board[row][col] = 'Q';
                 before[row] = 1;
                 upDi[row + col] = 1;
                 loDi[(n-1) + (col - row) ] = 1;
                 
-                f(before, upDi, loDi, col + 1, n);
+                count = f(before, upDi, loDi, col + 1, n, count);
                 
-                // board[row][col] = 'Q';
                 before[row] = 0;
                 upDi[row + col] = 0;
                 loDi[(n-1) + (col - row) ] = 0;
             }
         }
+        
+        return count;
     }
 }
