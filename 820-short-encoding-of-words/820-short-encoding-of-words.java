c420b9aab3fe04f8dd508900b85c7c5f4a39ba6e
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2022-06-20 22:21:11 +0530

    Time: 728 ms (5.52%), Space: 49.6 MB (83.44%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Arrays.sort(words, (a, b) -> b.length() - a.length());
+        StringBuilder sb = new StringBuilder();
+        for(String s: words) {
+            if(sb.indexOf(s + '#') == -1) {
+                sb.append(s + '#');
+            }
+        }
+        
+        return sb.length();
+    }
+    
+}
