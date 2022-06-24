54a6814c58a0f8b8baf9541f4ea7cd073fbf320b
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2022-06-25 00:00:31 +0530

    Time: 63 ms (5.05%), Space: 57.3 MB (72.73%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,49 +1,28 @@
 class Solution {
-//     public boolean isPossible(int[] target) {
-//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(target.length, (a, b) -> b - a);
-//         int sum = 0;
-//         for(int el: target) {
-//             maxHeap.add(el);
-//             sum += el;
-//         }
-        
-//         while (maxHeap.peek() != 1) {
-// 		    int curr = maxHeap.poll();
-// 		    if(sum - curr == 1) return true;
-
-// 		    int x = curr % (sum - curr);
-//             System.out.println("x("+ x +") = curr("+ curr +") % (sum("+ sum +") - curr("+ curr +"))");
-// 		    sum = sum - curr + x;
-//             System.out.println("sum = " + sum);
-
-// 		    if (x == 0 || x == curr) return false;
-// 		    else maxHeap.add(x);
-// 	    }
-
-// 	    return true;
-//     }
-    
     public boolean isPossible(int[] target) {
-	if (target.length == 1) return target[0] == 1;
+        if (target.length == 1) return target[0] == 1;
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(target.length, (a, b) -> b - a);
+        int sum = 0;
+        for(int el: target) {
+            maxHeap.add(el);
+            sum += el;
+        }
+        
+        while (maxHeap.peek() != 1) {
+		    int curr = maxHeap.poll();
+		    if(sum - curr == 1) return true;
 
-	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-	int sum = 0;
-	for (int t: target) {
-		pq.add(t);
-		sum += t;
-	}
+		    int x = curr % (sum - curr);
+            System.out.println("x("+ x +") = curr("+ curr +") % (sum("+ sum +") - curr("+ curr +"))");
+		    sum = sum - curr + x;
+            System.out.println("sum = " + sum);
 
-	while (pq.peek() != 1) {
-		int curr = pq.poll();
-		if (sum - curr == 1) return true;
+		    if (x == 0 || x == curr) return false;
+		    else maxHeap.add(x);
+	    }
 
-		int x = curr % (sum - curr);
-		sum = sum - curr + x;
+	    return true;
+    }
 
-		if (x == 0 || x == curr) return false;
-		else pq.add(x);
-	}
-
-	return true;
-}
 }
