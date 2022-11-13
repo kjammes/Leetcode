fac04f37f16bc0751145fd1d09ce125c9812ff18
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2022-11-13 15:55:18 +0530

    Time: 13 ms (66.12%), Space: 43.7 MB (55.34%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,29 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
-      if(s == null)
-        return s;
-
-      s = s.trim();
-      StringBuilder sb = new StringBuilder();
-      List<String> tokens = new ArrayList<>();
-      for(int i = 0; i < s.length();) {
-        if(s.charAt(i) == ' ') {
-          System.out.println(sb);
-          tokens.add(sb.toString());
-          sb = new StringBuilder("");
-          while(s.charAt(i) == ' ')
-            i++;
+        s=s.trim();
+        String str[]=s.split("\\s+");
+        StringBuilder sb=new StringBuilder();
+       
+        for(int i=str.length-1;i>=0;i--){
+            sb.append(str[i]);
+            if(i!=0)
+                sb.append(" ");
         }
-        sb.append(s.charAt(i++));
-      }
-      System.out.println(sb);
-      if(tokens.size() == 0)
         return sb.toString();
-
-      for(int i = tokens.size() - 1; i >= 0; i--) {
-        sb.append(" " + tokens.get(i));
-      }
-
-      return sb.toString();
     }
 }
