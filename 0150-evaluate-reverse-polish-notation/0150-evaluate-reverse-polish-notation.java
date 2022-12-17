1bc3778f7ca8722855350790ee451cdcffc188bd
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2022-12-17 20:39:08 +0530

    Time: 9 ms (76.51%), Space: 44.6 MB (22.95%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        Set<String> ops = new HashSet<>(Arrays.asList("*", "/", "+", "-"));
+        
+        for(String token: tokens) {
+            if(!ops.contains(token)) {
+                stack.push(Integer.valueOf(token));
+            } else {
+                int a = stack.pop(), b = stack.pop();
+                int result = switch (token) {
+    case "+" -> b + a;
+    case "-" -> b - a;
+    case "*" -> a * b;
+    case "/" -> b / a;
+    default -> throw new IllegalArgumentException("Invalid operator: " + token);
+};
+stack.push(result);
+            }
+        }
+        
+        return stack.peek();
+    }
+}
