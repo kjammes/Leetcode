95835a2dc7c4d57a9f54595fe7c57dfe35bc4525
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-01-04 21:08:29 +0530

    Time: 59 ms (59.30%), Space: 107.5 MB (21.74%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -3,24 +3,21 @@ class Solution {
         if(tasks.length <= 1)
             return -1;
         
-        Map<Integer, Long> map = Arrays.stream(tasks)
-            .boxed()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println(map);
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int t : tasks) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+
         
         int result = 0;
-        for(Map.Entry<Integer, Long> entry : map.entrySet()) {
-            long val = entry.getValue();
-            if (val == 1) return -1;
-            
-            result += val / 3;
-            System.out.println(val / 3);
-            
-            System.out.println(val % 3);
-            if (val % 3 != 0) result++;
+        for (int count : map.values()) {
+            if (count == 1) return -1;
+
+            result += count / 3;
+
+            if(count % 3 != 0) result++;
         }
-        
-        System.out.println(map);
         
         return result;
     }
