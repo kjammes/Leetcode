b83773de8bac1031a2ee013ba6f153592e340e08
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-02-04 19:57:49 +0530

    Time: 5 ms (80.89%), Space: 42.4 MB (65.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,49 +1,43 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c: s1.toCharArray()) {
-            int freq = 0;
-            if (map.containsKey(c)) {
-                freq = map.get(c);
-            }
-            freq++;
-            map.put(c, freq);
+        int len1 = s1.length();
+        int len2 = s2.length();
+        
+        if (len1 > len2) 
+            return false;
+        
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        
+        for (int i = 0; i < len1; i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
         }
         
-        int left = 0, right = s1.length() - 1;
-        while(left < s2.length() && right < s2.length()) {
-            Map<Character, Integer> temp = new HashMap<>();
-            int pointer = left;
-            while (pointer < s2.length() && pointer <= right) {
-                char cur = s2.charAt(pointer);
-                int curFreq = 0;
-                if (temp.containsKey(cur)) {
-                    curFreq = temp.get(cur);
-                }
-                curFreq++;
-                temp.put(cur, curFreq);
-                pointer++;
-            }
-            
-            left++;
-            right++;
-            
-            if (map.size() != temp.size())
-                continue;
-            
-            boolean allMatching = true;
-            for (Character c: map.keySet()) {
-                if ( !temp.containsKey(c) || !temp.get(c).equals(map.get(c)) ) {
-                    allMatching = false;
-                    break;
-                }
-            }
-            
-            if (allMatching)
+        int matches = 0;
+        for (int i = 0; i < 26; i++)
+            matches += s1Count[i] == s2Count[i] ? 1 : 0;
+        
+        int left = 0;
+        for (int right = len1; right < len2; right++) {
+            if (matches == 26)
                 return true;
             
+            int idx = s2.charAt(right) - 'a';
+            s2Count[idx]++;
+            if (s2Count[idx] == s1Count[idx])
+                matches++;
+            else if(s2Count[idx] == s1Count[idx] + 1)
+                matches--;
+            
+            idx = s2.charAt(left++) - 'a';
+            s2Count[idx]--;
+            if (s2Count[idx] == s1Count[idx])
+                matches++;
+            else if (s2Count[idx] == s1Count[idx] - 1)
+                matches--;
         }
         
-        return false;
+        return matches == 26;
     }
 }
