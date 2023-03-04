a2cadab867973e263e0835f65681ef528e43521b
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-03-04 20:10:23 +0530

    Time: 9 ms (95.24%), Space: 50.6 MB (59.89%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,17 +1,22 @@
 class Solution {
     public int numberOfSubarrays(int[] A, int k) {
-        int res = 0, i = 0, count = 0, n = A.length;
-        for (int j = 0; j < n; j++) {
-            if (A[j] % 2 == 1) {
-                --k;
+        int res = 0;
+        int count = 0;
+        int p = 0;
+        
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 1) {
+                k--;
                 count = 0;
             }
+                
             while (k == 0) {
-                k += A[i++] & 1;
-                ++count;
+                k += A[p++] & 1;
+                count++;
             }
             res += count;
         }
+        
         return res;
     }
 }
