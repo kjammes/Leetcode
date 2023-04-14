71bd086b2f9ea2bd554133c3ca69ada3526cddc0
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-04-14 17:05:53 +0530

    Time: 31 ms (92.59%), Space: 52.5 MB (14.24%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -3,49 +3,13 @@ class Solution {
     int[][] dp;
     String s;
 
-    // public int longestPalindromeSubseq(String s) {
-    //     int len = s.length();        
-    //     // dp = new int[len][len];
-    //     // this.s = s;
-
-    //     // return f(0, len -1);
-
-    //     int dp[] = new int[len];
-    //     for (int i = len - 1; i >= 0; i--) {
-    //         int[] tempDP = new int[len];
-    //         dp[i] = 1;
-    //         for (int j = i + 1; j < len; j++) {
-    //             if (s.charAt(i) == s.charAt(j))
-    //                 tempDP[j] = 2 + dp[j - 1];
-    //             else
-    //                 tempDP[j] = Math.max(dp[j], tempDP[j - 1]);
-    //         }
-    //         dp = tempDP;
-    //     }
-    //     return dp[len - 1];
-    // }
-
     public int longestPalindromeSubseq(String s) {
         int len = s.length();        
-        int[] dp = new int[len];
-        for (int i = len - 1; i >= 0; i--) {
-            int prev = 0; // variable to keep track of dp[j-1] from previous iteration
-            dp[i] = 1; // initialize dp[i] as 1 since a single character is always a palindrome
-            for (int j = i + 1; j < len; j++) {
-                int temp = dp[j]; // temporarily store dp[j] as it will be updated
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[j] = prev + 2; // update dp[j] with dp[j-1] + 2
-                } else {
-                    dp[j] = Math.max(dp[j], dp[j - 1]); // update dp[j] with max of dp[j] and dp[j-1]
-                }
-                prev = temp; // update prev with the previous value of dp[j]
-            }
-        }
-        return dp[len - 1]; // dp[len - 1] contains the length of the longest palindromic subsequence
+        dp = new int[len][len];
+        this.s = s;
+        return f(0, len -1);
     }
-
-
-
+    
     private int f(int l, int r) {
         if (l > r)
             return 0;
@@ -65,4 +29,31 @@ class Solution {
 
         return dp[l][r];
     }
+
+    // public int longestPalindromeSubseq(String s) {
+    //     int len = s.length();        
+    //     int[] dp = new int[len];
+    //     for (int i = len - 1; i >= 0; i--) {
+    //         System.out.println("i is " + i);
+    //         int prev = 0; // variable to keep track of dp[j-1] from previous iteration
+    //         dp[i] = 1; // initialize dp[i] as 1 since a single character is always a palindrome
+    //         for (int j = i + 1; j < len; j++) {
+    //             System.out.println("j is " + j);
+    //             int temp = dp[j]; // temporarily store dp[j] as it will be updated
+    //             System.out.println("temp : " + temp);
+    //             System.out.println("prev : " + prev);
+    //             if (s.charAt(i) == s.charAt(j)) {
+    //                 System.out.println("In if");
+    //                 dp[j] = prev + 2; // update dp[j] with dp[j-1] + 2
+    //             } else {
+    //                 System.out.println("In else");
+    //                 dp[j] = Math.max(dp[j], dp[j - 1]); // update dp[j] with max of dp[j] and dp[j-1]
+    //             }
+    //             System.out.println("dp[j] : " + dp[j]);
+    //             prev = temp; // update prev with the previous value of dp[j]
+    //         }
+    //         System.out.println();
+    //     }
+    //     return dp[len - 1]; // dp[len - 1] contains the length of the longest palindromic subsequence
+    // }
 }
