4e7aee4b364c85677b5205e168ac5ea9131af3f1
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-04-16 16:18:21 +0530

    Time: 95 ms (43.72%), Space: 48.8 MB (97.81%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,43 +1,26 @@
 class Solution {
-
-    long mod = 1000000000 + 7;
-    int dp[][];
-    int wordLen;
-    int targetLen;
-
     public int numWays(String[] words, String target) {
-        wordLen = words[0].length();
-        targetLen = target.length();
-        dp = new int[wordLen][targetLen];
-        for (int i = 0; i < wordLen; i++)
-            Arrays.fill(dp[i], -1);
-
-        Map<String, Integer> cnt = new HashMap<>();
+        int wLen = words[0].length();
+        int tLen = target.length();
+        int mod = 1000000007;
+        int[] dp = new int[tLen+1];
+        dp[0] = 1;
+        
+        int[][] count = new int[wLen][26];
         for (String word: words) {
-            for (int i = 0; i < wordLen; i++) {
-                String key = String.valueOf(i + "_" + word.charAt(i));
-                cnt.put(key, cnt.getOrDefault(key, 0) + 1);
+            for (int i = 0; i < wLen; i++) {
+                count[i][word.charAt(i) - 'a']++;
             }
         }
         
-        return f(words, target, 0, 0, cnt);
-    }
 
-    private int f(String[] words, String target, int wIdx, int tIdx, Map<String, Integer> cnt) {
-        if (tIdx == targetLen)
-            return 1;
+        for (int i = 0; i < wLen; i++) {
+            for (int j = tLen - 1; j >= 0; j--) {
+                dp[j + 1] += (int)((long)dp[j] * count[i][target.charAt(j) - 'a']  % mod);
+                dp[j+1] %= mod;
+            }
+        }
 
-        if (wIdx == wordLen)
-            return 0;
-
-        if (dp[wIdx][tIdx] != -1)
-            return dp[wIdx][tIdx];
-
-        long result = f(words, target, wIdx + 1, tIdx, cnt);
-        String key = String.valueOf(wIdx + "_" + target.charAt(tIdx));
-        result += ((long) cnt.getOrDefault(key, 0) * f(words, target, wIdx + 1, tIdx + 1, cnt));
-
-        dp[wIdx][tIdx] = (int) (result % mod);
-        return dp[wIdx][tIdx];
+        return dp[tLen];
     }
 }
