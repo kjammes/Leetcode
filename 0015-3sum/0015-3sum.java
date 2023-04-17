7f616579dcbab4f1445f139defcb3d77fcfdfd3a
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-04-17 17:45:06 +0530

    Time: 42 ms (63.14%), Space: 49.9 MB (61.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,25 +4,26 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || nums[i] != nums[i-1]) {
-                int left = i + 1;
-                int right = nums.length - 1;
-                while (left < right) {
-                    int res = nums[i] + nums[left] + nums[right];
-                    if (res == 0) {
-                        result.add(List.of(nums[i], nums[left], nums[right]));
+            if (i != 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int res = nums[i] + nums[left] + nums[right];
+                if (res == 0) {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
                         
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-                        
-                        left++;
-                        right--;
-                    } else if (res > 0) 
-                        right--;
-                    else
-                        left++;
-                }
-            }
+                    left++;
+                    right--;
+                } else if (res > 0) 
+                    right--;
+                else
+                    left++;
+            }    
         }
         
         return result;
