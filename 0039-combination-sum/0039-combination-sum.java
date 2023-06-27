55c4f911afbcb61aea4cb8ed314be7ce619e7e6e
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-06-27 23:38:17 +0530

    Time: 2 ms (81.84%), Space: 44.3 MB (14.83%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,12 +1,12 @@
 class Solution {
-    List<List<Integer>> res;
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        res = new ArrayList<>();
-        f(candidates, target, new ArrayList<>(), 0, 0);
+        List<List<Integer>> res = new ArrayList<>();
+        f(candidates, target, new ArrayList<>(), 0, 0, res);
         return res;
     }
 
-    private void f(int[] candidates, int target, List<Integer> cur, int idx, int sum) {
+    private void f(int[] candidates, int target, List<Integer> cur, int idx, int sum, List<List<Integer>> res) {
         if (sum > target) return;
 
         if (sum == target) {
@@ -17,8 +17,8 @@ class Solution {
         if (idx >= candidates.length) return;
 
         cur.add(candidates[idx]);
-        f(candidates, target, cur, idx, sum + candidates[idx]);
+        f(candidates, target, cur, idx, sum + candidates[idx], res);
         cur.remove(cur.size() - 1);
-        f(candidates, target, cur, idx + 1, sum);
+        f(candidates, target, cur, idx + 1, sum, res);
     }
 }
