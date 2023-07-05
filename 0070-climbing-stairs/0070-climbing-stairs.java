c7141e99b729795254c1f4cb20278a6211a1bca5
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-05 23:05:05 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (95.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,29 @@
 class Solution {
     public int climbStairs(int n) {
-        int cache[] = new int[n];
-        return f(n - 1, cache);
+        // int cache[] = new int[n];
+        // return f(n - 1, cache);
+        
+        int x = 1;
+        int y = 1;
+        int z;
+        for (int i = 0; i < n - 1; i++) {
+            z = x + y;
+            x = y;
+            y = z;
+        }
+        return y;
     }
     
-    private int f(int n, int[] cache) {
-        if (cache[n] != 0)
-            return cache[n];
+//     private int f(int n, int[] cache) {
+//         if (cache[n] != 0)
+//             return cache[n];
         
-        if (n <= 2) {
-            cache[n] = n + 1;
-            return cache[n];
-        }
+//         if (n <= 2) {
+//             cache[n] = n + 1;
+//             return cache[n];
+//         }
         
-        cache[n] = f(n - 1, cache) + f(n - 2, cache);
-        return cache[n];
-    }
+//         cache[n] = f(n - 1, cache) + f(n - 2, cache);
+//         return cache[n];
+//     }
 }
