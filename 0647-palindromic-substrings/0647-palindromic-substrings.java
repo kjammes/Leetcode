9d98dfe498c01966211aa6cde0ad947d4dbbd50c
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-09 14:57:02 +0530

    Time: 15 ms (38.98%), Space: 43.9 MB (22.89%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,13 +1,42 @@
 class Solution {
-    public int countSubstrings(String s) {
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int left = i, right = i;
-            while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) { res++; }
+//     public int countSubstrings(String s) {
+//         int res = 0;
+//         for (int i = 0; i < s.length(); i++) {
+//             int left = i, right = i;
+//             while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) { res++; }
             
-            left = i; right = i + 1;
-            while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) { res++; }
+//             left = i; right = i + 1;
+//             while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) { res++; }
+//         }
+//         return res;
+//     }
+    public int countSubstrings(String s) {
+        
+        int len = s.length();
+        
+        var dp = new boolean[len][len];
+        for (int i = 0; i < len; i++)
+            dp[i][i] = true;
+        
+        int res = len;
+        for (int i = 0; i < len - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1))
+                continue;
+            dp[i][i+1] = true;
+            res++;
         }
+            
+        
+        for (int sze = 2; sze < len; sze++) {
+            for (int i = 0; i + sze < len; i++) {
+                int j = i + sze;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    res++;
+                    dp[i][j] = true;
+                }
+            } 
+        }
+        
         return res;
     }
 }
