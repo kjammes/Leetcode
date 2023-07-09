29b284186a31fd8ec5a091d753d1f9ff3558c992
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-09 20:27:17 +0530

    Time: 1 ms (95.21%), Space: 40.4 MB (98.72%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,7 +1,22 @@
 class Solution {
     
     public int numDecodings(String s) {
-        return f(s, 0, new Integer[s.length()]);
+        // return f(s, 0, new Integer[s.length()]);
+        int len = s.length();
+        int dp[] = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= len; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            
+            if (s.charAt(i - 2) == '1' || s.charAt(i-2) == '2' && s.charAt(i-1) < '7') {
+                dp[i] += dp[i-2];
+            }
+        }
+        
+        return dp[len];
     }
     
     private Integer f(String s, int idx, Integer[] dp) {
