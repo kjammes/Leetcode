d8b2fe4a01e3b0150741c73cb04c7fa6431b5a82
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-09 23:05:44 +0530

    Time: 17 ms (79.02%), Space: 43 MB (84.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,51 @@
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int dp[] = new int[amount + 1];
+//         Arrays.fill(dp, -1);
+//         int result = coinChangeRecursive(coins, amount, dp);
+//         return result == Integer.MAX_VALUE ? -1 : result;
+//     }
+    
+//     private int coinChangeRecursive(int[] coins, int amount, int[] dp) {
+//         if (amount == 0) {
+//             return 0;
+//         }
+        
+//         if (amount < 0) {
+//             return Integer.MAX_VALUE;
+//         }
+        
+//         if (dp[amount] != -1) 
+//             return dp[amount];
+        
+//         int minCoins = Integer.MAX_VALUE;
+//         for (int i = 0; i < coins.length; i++) {
+//             int subproblem = coinChangeRecursive(coins, amount - coins[i], dp);
+//             if (subproblem != Integer.MAX_VALUE) {
+//                 minCoins = Math.min(minCoins, 1 + subproblem);
+//             }
+//         }
+        
+//         dp[amount] = minCoins;
+//         return minCoins;
+//     }
+// }
+
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[] = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        int result = coinChangeRecursive(coins, amount, dp);
-        return result == Integer.MAX_VALUE ? -1 : result;
-    }
-    
-    private int coinChangeRecursive(int[] coins, int amount, int[] dp) {
-        if (amount == 0) {
-            return 0;
-        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
         
-        if (amount < 0) {
-            return Integer.MAX_VALUE;
-        }
-        
-        if (dp[amount] != -1) 
-            return dp[amount];
-        
-        int minCoins = Integer.MAX_VALUE;
-        for (int i = 0; i < coins.length; i++) {
-            int subproblem = coinChangeRecursive(coins, amount - coins[i], dp);
-            if (subproblem != Integer.MAX_VALUE) {
-                minCoins = Math.min(minCoins, 1 + subproblem);
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
             }
         }
         
-        dp[amount] = minCoins;
-        return minCoins;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
