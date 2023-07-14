ed4cedd4c11cc679cc2d31c0af8d41fa51dad9ee
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-14 23:34:48 +0530

    Time: 32 ms (88.07%), Space: 40.6 MB (98.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,33 +1,23 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
+        int sum = 0;
         for (int num : nums)
-            totalSum += num;
+            sum += num;
 
         // If the total sum is odd, it cannot be divided equally
-        if (totalSum % 2 != 0)
+        if (sum % 2 != 0)
             return false;
 
-        int targetSum = totalSum / 2;
-        Map<String, Boolean> memo = new HashMap<>();
-        return canPartitionUtil(nums, targetSum, 0, 0, 0, memo);
-    }
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
 
-    private boolean canPartitionUtil(int[] nums, int targetSum, int currSum1, int currSum2, int idx, Map<String, Boolean> memo) {
-        if (currSum1 == targetSum || currSum2 == targetSum)
-            return true;
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
 
-        if (currSum1 > targetSum || currSum2 > targetSum)
-            return false;
-
-        String key = currSum1 + "-" + currSum2;
-        if (memo.containsKey(key))
-            return memo.get(key);
-
-        boolean foundPartition = canPartitionUtil(nums, targetSum, currSum1 + nums[idx], currSum2, idx + 1, memo) ||
-                                 canPartitionUtil(nums, targetSum, currSum1, currSum2 + nums[idx], idx + 1, memo);
-
-        memo.put(key, foundPartition);
-        return foundPartition;
+        return dp[target];
     }
 }
