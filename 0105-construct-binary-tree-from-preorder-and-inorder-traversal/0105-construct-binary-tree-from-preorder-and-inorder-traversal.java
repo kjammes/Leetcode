49bc2aa4baa08b232ccce083699ed3440ccbb768
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-16 14:07:05 +0530

    Time: 13 ms (6.89%), Space: 80.7 MB (5.50%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,40 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    Map<Integer, Integer> inorderPositions = new HashMap<>();
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length < 1 || inorder.length < 1) return null;
+        if (preorder.length == 0 || inorder.length == 0)
+            return null;
 
+        int rootVal = preorder[0];
+        int idx = 0;
         for (int i = 0; i < inorder.length; i++) {
-            inorderPositions.put(inorder[i], i);
+            if (inorder[i] == rootVal)
+                idx = i;
         }
+        TreeNode root = new TreeNode(rootVal);
+        root.left = buildTree(
+            Arrays.copyOfRange(preorder, 1, idx + 1),
+            Arrays.copyOfRange(inorder, 0, idx)
+        );
+        root.right = buildTree(
+            Arrays.copyOfRange(preorder, idx + 1, preorder.length),
+            Arrays.copyOfRange(inorder, idx + 1, inorder.length)
+        );
 
-        return builder(preorder, 0, 0, inorder.length - 1);
-    }
-
-    public TreeNode builder(
-        int[] preorder,
-        int preorderIndex,
-        int inorderLow,
-        int inorderHigh
-    ) {
-        if (
-            preorderIndex > preorder.length - 1 || inorderLow > inorderHigh
-        ) return null;
-
-        int currentVal = preorder[preorderIndex];
-        TreeNode n = new TreeNode(currentVal);
-        int mid = inorderPositions.get(currentVal);
-
-        n.left = builder(preorder, preorderIndex + 1, inorderLow, mid - 1);
-        n.right =
-            builder(
-                preorder,
-                preorderIndex + (mid - inorderLow) + 1,
-                mid + 1,
-                inorderHigh
-            );
-
-        return n;
+        return root;
     }
 }
