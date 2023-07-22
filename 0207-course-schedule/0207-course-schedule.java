a97fc2af2db3016125e1a172b7a6ab1a72fac8d4
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-22 23:59:02 +0530

    Time: 22 ms (8.92%), Space: 43.9 MB (78.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,15 +2,9 @@ import java.util.*;
 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
         int[] indegree = new int[numCourses];
-
         for (int[] el : prerequisites) {
-            int course = el[0];
-            int prerequisite = el[1];
-            adj.putIfAbsent(prerequisite, new ArrayList<>());
-            adj.get(prerequisite).add(course);
-            indegree[course]++;
+            indegree[el[0]]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
@@ -24,11 +18,11 @@ class Solution {
             int node = q.poll();
             count++;
 
-            if (adj.containsKey(node)) {
-                for (int course : adj.get(node)) {
-                    indegree[course]--;
-                    if (indegree[course] == 0)
-                        q.offer(course);
+            for (int[] el : prerequisites) {
+                if (el[1] == node) {
+                    indegree[el[0]]--;
+                    if (indegree[el[0]] == 0)
+                        q.offer(el[0]);
                 }
             }
         }
