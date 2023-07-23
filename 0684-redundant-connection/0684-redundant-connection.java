2388cc606af3590ea86595068ed77837879c3538
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-23 21:04:55 +0530

    Time: 1 ms (74.42%), Space: 43.5 MB (56.33%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,27 +1,57 @@
 class Solution {
-
-    int[] parent;
-
     public int[] findRedundantConnection(int[][] edges) {
-        parent = new int[edges.length];
-        for (int i = 0; i < edges.length; i++) parent[i] = i + 1;
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n);
 
         for (int[] edge : edges) {
-            if (find(edge[0]) == find(edge[1])) return edge; else union(
-                edge[0],
-                edge[1]
-            );
+            int x = edge[0], y = edge[1];
+            if (uf.isConnected(x, y)) {
+                return edge;
+            }
+            uf.union(x, y);
         }
 
-        return new int[2];
+        return new int[0];
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    UnionFind(int n) {
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
     }
 
-    public int find(int x) {
-        if (x == parent[x - 1]) return x;
-        return find(parent[x - 1]);
+    int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
     }
 
-    public void union(int x, int y) {
-        parent[find(y) - 1] = find(x);
+    void union(int x, int y) {
+        int xRoot = find(x);
+        int yRoot = find(y);
+        if (xRoot == yRoot) {
+            return;
+        }
+
+        if (rank[xRoot] > rank[yRoot]) {
+            parent[yRoot] = xRoot;
+        } else if (rank[xRoot] < rank[yRoot]) {
+            parent[xRoot] = yRoot;
+        } else {
+            parent[yRoot] = xRoot;
+            rank[xRoot]++;
+        }
+    }
+
+    boolean isConnected(int x, int y) {
+        return find(x) == find(y);
     }
 }
