c852d6bf5a5d552713ff11b1f0b9c8cd5f14c86b
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-26 21:50:02 +0530

    Time: 1 ms (84.16%), Space: 40.6 MB (69.39%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,6 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return f(prices, 0, true, new HashMap<>());
+        // return f(prices, 0, true, new HashMap<>());
+        int n = prices.length;
+        if (n <= 1) return 0;
+
+        int[] buy = new int[n]; // Maximum profit on day i with a stock in hand
+        int[] sell = new int[n]; // Maximum profit on day i with no stock in hand
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        sell[1] = Math.max(0, prices[1] - prices[0]);
+
+        for (int i = 1; i < n; i++) {
+            // The maximum profit on day i with a stock in hand is either:
+            // 1. Continue holding the stock (buy[i-1])
+            // 2. Buy the stock on day i (sell[i-2] - prices[i])
+            buy[i] = Math.max(buy[i - 1], (i >= 2 ? sell[i - 2] : 0) - prices[i]);
+
+            // The maximum profit on day i with no stock in hand is either:
+            // 1. Continue not holding the stock (sell[i-1])
+            // 2. Sell the stock on day i (buy[i-1] + prices[i])
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+        }
+
+        return sell[n - 1];
     }
     
     private int f(int[] prices, int idx, boolean canBuy, Map<String, Integer> cache) {
