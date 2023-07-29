c487455b9898d960e278b80284d27d5871a10f12
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-29 18:13:20 +0530

    Time: 2 ms (41.14%), Space: 46.4 MB (8.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,16 +2,11 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         
         int m = isConnected.length;
-        List<Integer>[] graph = new List[m];
         UnionFind uf = new UnionFind(m);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
                 if (isConnected[i][j] == 0)
                     continue;
-                // if (graph[i] == null) graph[i] = new ArrayList<>();
-                // if (graph[j] == null) graph[j] = new ArrayList<>();
-                // graph[i].add(j);
-                // graph[j].add(i);
                 uf.union(i, j);
             }
         }
@@ -20,25 +15,11 @@ class Solution {
         int res = 0;
         Set<Integer> pars = new HashSet<>();
         for (int i = 0; i < m; i++) {
-            // if (visited[i])
-            //     continue;
-            // res++;
-            // dfs(graph, i, visited);
-            pars.add(uf.find(i));
+            int uPar = uf.find(i);
+            if (!pars.contains(uPar))
+                pars.add(uPar);
         }
         return pars.size();
-    }
-    
-    private void dfs(List<Integer>[] graph, int node,boolean[] visited) {
-        if (visited[node])
-            return;
-        
-        visited[node] = true;
-        for (int n: graph[node]) {
-            if (visited[n])
-                continue;
-            dfs(graph, n, visited);
-        }
     }
 }
 
