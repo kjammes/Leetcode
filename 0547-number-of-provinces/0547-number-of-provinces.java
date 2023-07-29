4d499fe12210fae2d1a5b11f547bbf95e19bd176
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-29 18:11:59 +0530

    Time: 3 ms (27.26%), Space: 46 MB (22.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,26 +3,30 @@ class Solution {
         
         int m = isConnected.length;
         List<Integer>[] graph = new List[m];
+        UnionFind uf = new UnionFind(m);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
                 if (isConnected[i][j] == 0)
                     continue;
-                if (graph[i] == null) graph[i] = new ArrayList<>();
-                if (graph[j] == null) graph[j] = new ArrayList<>();
-                graph[i].add(j);
-                graph[j].add(i);
+                // if (graph[i] == null) graph[i] = new ArrayList<>();
+                // if (graph[j] == null) graph[j] = new ArrayList<>();
+                // graph[i].add(j);
+                // graph[j].add(i);
+                uf.union(i, j);
             }
         }
         
         boolean visited[] = new boolean[m];
         int res = 0;
+        Set<Integer> pars = new HashSet<>();
         for (int i = 0; i < m; i++) {
-            if (visited[i])
-                continue;
-            res++;
-            dfs(graph, i, visited);
+            // if (visited[i])
+            //     continue;
+            // res++;
+            // dfs(graph, i, visited);
+            pars.add(uf.find(i));
         }
-        return res;
+        return pars.size();
     }
     
     private void dfs(List<Integer>[] graph, int node,boolean[] visited) {
@@ -35,5 +39,34 @@ class Solution {
                 continue;
             dfs(graph, n, visited);
         }
+    }
+}
+
+class UnionFind {
+    int[] rank;
+    int[] parent;
+    
+    UnionFind(int n) {
+        this.rank = new int[n];
+        this.parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    void union(int m, int n) {
+        int mPar = find(m), nPar = find(n);
+        if (rank[mPar] > rank[nPar]) {
+            parent[nPar] = mPar;
+        } else if (rank[mPar] < rank[nPar]) {
+            parent[mPar] = nPar;
+        } else {
+            parent[mPar] = nPar;
+            rank[nPar]++;
+        }
+    }
+    
+    int find(int x) {
+        if (parent[x] == x) return x;
+        parent[x] = find(parent[x]);
+        return parent[x];
     }
 }
