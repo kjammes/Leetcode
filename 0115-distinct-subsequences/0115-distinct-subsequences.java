1878b0e22e9abce2009ab436930afe20e5f4886b
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-07-30 14:37:37 +0530

    Time: 15 ms (91.01%), Space: 47.1 MB (55.56%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,35 +1,28 @@
 class Solution {
-    
-    int sLen, tLen;
-    
     public int numDistinct(String s, String t) {
-        sLen = s.length(); tLen = t.length();
-        if (tLen > sLen)
-            return 0;
-        if (tLen == sLen)
-            return s.equals(t) ? 1 : 0;
-        int[][] dp = new int[sLen][tLen];
-        for (int r[]: dp)
-            Arrays.fill(r, -1);
-        return f(s, t, 0, 0, dp);
-    }
-    
-    private int f(String s, String t, int idxS, int idxT, int[][] dp) {
-        if (idxT == tLen)
-            return 1;
+        int sLen = s.length();
+        int tLen = t.length();
         
-        if (idxS >= sLen || idxT >= tLen)
-            return 0;
+        // dp[i][j] will store the number of distinct subsequences of s[0...i-1] which equals t[0...j-1].
+        int[][] dp = new int[sLen + 1][tLen + 1];
         
-        if (dp[idxS][idxT] != -1)
-            return dp[idxS][idxT];
+        // For any i, dp[i][0] should be 1 because there is one way to form an empty string from any non-empty string.
+        for (int i = 0; i <= sLen; i++)
+            dp[i][0] = 1;
         
-        int skip = f(s, t, idxS + 1, idxT, dp);
-        int choose = 0;
-        if (s.charAt(idxS) == t.charAt(idxT)) 
-            choose = f(s, t, idxS + 1, idxT + 1, dp);
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= tLen; j++) {
+                // If the characters match, we have two choices:
+                // 1. We can include the current character in both s and t to form the subsequence.
+                // 2. We can exclude the current character from s and consider the previous subsequence.
+                if (s.charAt(i - 1) == t.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else
+                    // If the characters don't match, we can only exclude the current character from s.
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
         
-        dp[idxS][idxT] = skip + choose;
-        return dp[idxS][idxT];
+        return dp[sLen][tLen];
     }
 }
