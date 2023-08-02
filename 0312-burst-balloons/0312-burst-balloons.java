48caf06968d6573fe5149877f4d519637326fbbc
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-02 23:48:59 +0530

    Time: 104 ms (60.89%), Space: 41.5 MB (76.46%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        int[] numsWithBoundary = new int[nums.length + 2];
-        numsWithBoundary[0] = numsWithBoundary[nums.length + 1] = 1;
-        for (int i = 0; i < nums.length; i++) {
-            numsWithBoundary[i + 1] = nums[i];
-        }
-        int dp[][] = new int[nums.length + 2][nums.length + 2];
-        return burstBalloons(numsWithBoundary, 0, numsWithBoundary.length - 1, dp);
+        int dp[][] = new int[nums.length][nums.length];
+        return burstBalloons(nums, 0, nums.length - 1, dp);
     }
 
     private int burstBalloons(int[] nums, int left, int right, int[][] dp) {
-        if (left + 1 == right) {
+        if (left > right)
             return 0;
-        }
         
         if (dp[left][right] != 0)
             return dp[left][right];
-
-        int maxCoins = 0;
-        for (int i = left + 1; i < right; i++) {
-            int coins = nums[left] * nums[i] * nums[right] 
-                        + burstBalloons(nums, left, i, dp) 
-                        + burstBalloons(nums, i, right, dp);
-            maxCoins = Math.max(maxCoins, coins);
+        
+        int coins = 0;
+        for (int i = left; i <= right; i++) {
+            int curCoins = nums[i];
+            if (left - 1 >= 0) {
+                curCoins *= nums[left - 1];
+            }
+            if (right + 1 < nums.length) {
+                curCoins *= nums[right + 1];
+            }
+            curCoins += burstBalloons(nums, left, i - 1, dp) + burstBalloons(nums, i + 1, right, dp);
+            coins = Math.max(coins, curCoins);
         }
-
-        dp[left][right] = maxCoins;
-        return maxCoins;
+        
+        return dp[left][right] = coins;
     }
 }
