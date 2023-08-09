c762082c2fe13fbea05f83c4b112db4f2c1ef1e6
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-09 21:00:57 +0530

    Time: 33 ms (59.43%), Space: 51.4 MB (44.61%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -10,14 +10,20 @@ class Pair {
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Double.compare(a.distance, b.distance));
+        PriorityQueue<Pair> pq = new PriorityQueue<>(k, (a, b) -> Double.compare(b.distance, a.distance));
         
         for (int point[]: points) {
             double eDist = Math.sqrt(
                 Math.pow(0 - point[0], 2) +
                 Math.pow(0 - point[1], 2)
             );
-            pq.offer(new Pair(new int[] {point[0], point[1]}, eDist));
+            if (pq.size() < k) {
+                pq.offer(new Pair(new int[] {point[0], point[1]}, eDist));
+            } else if (pq.peek().distance > eDist) {
+                pq.poll();
+                pq.offer(new Pair(new int[] {point[0], point[1]}, eDist));
+            }
+            
         }
         
         int[][] res = new int[k][2];
