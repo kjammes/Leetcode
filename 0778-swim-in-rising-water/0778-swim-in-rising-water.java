99e0610e77f457df5836f1309b215dc884b8f58f
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-09 14:35:11 +0530

    Time: 18 ms (30.65%), Space: 43.1 MB (87.37%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -6,42 +6,62 @@ class Solution {
     
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-    
-        int low = 0, high = n * n - 1;        
-        while (low < high) {
+        
+        DSU  dsu = new DSU(n * n);
+        List<Integer> positions = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                positions.add(i * n + j);
+        Collections.sort(positions, (a, b) -> grid[a/n][a%n] - grid[b/n][b%n]);
+        
+        for (int pos: positions) {
+            int r = pos / n, c = pos % n;
             
-            int mid = low + (high - low) / 2;
-            boolean[][] visited = new boolean[n][n];
+            for (int[] dir: dirs) {
+                int nR = r + dir[0], nC = c + dir[1];
+                
+                if (nR >= 0 && nC >= 0 && nR < n && nC < n && grid[nR][nC] < grid[r][c])
+                    dsu.union(pos, nR * n + nC);
+            }
             
-            if (dfs(0, 0, grid, mid, visited)) {
-                high = mid;
-            } else
-                low = mid + 1;
+            if (dsu.find(0) == dsu.find(n * n - 1))
+                return grid[r][c];
         }
         
-        return low;
+        return -1;
+    }
+}
+
+class DSU {
+    int[] parent;
+    int[] rank;
+    
+    DSU(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) 
+            parent[i] = i;
+        rank = new int[n];
     }
     
-    private boolean dfs(int r, int c, int[][] grid, int target, boolean[][] visited) {
-        int n = grid.length;
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    
+    void union(int x, int y) {
+        int pX = find(x);
+        int pY = find(y);
         
-        if ( r < 0 || c < 0 || r >= n || c >= n || visited[r][c] || grid[r][c] > target)
-            return false;
-        
-        visited[r][c] = true;
-        
-        if (r == n - 1 && c == n - 1)
-            return true;
-        
-        boolean res = false;
-        for (int dir[]: dirs) {
-            int x = r + dir[0], y = c + dir[1];
-            if (dfs(x, y, grid, target, visited)) {
-                res = true;
-                break;
-            }
+        if (rank[pX] > rank[pY]) {
+            parent[pY] = pX;
+        } else if (rank[pX] < rank[pY]) {
+            parent[pX] = pY;
+        } else {
+            parent[pX] = pY;
+            rank[pX]++;
         }
-        
-        return res;
     }
 }
