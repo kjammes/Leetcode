e70809665a68b662929db027c9103ea33cb199aa
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-11 01:06:31 +0530

    Time: 82 ms (97.55%), Space: 68 MB (32.03%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -4,18 +4,27 @@ class MedianFinder {
     PriorityQueue<Integer> maxHeap;
 
     public MedianFinder() {
+        // We are basically dividing our input stream into two parts.
+        // the left part will always be smaller and we will need largest from that part (maxHeap)
+        // the right part will always be bigger and we will need smallest from that part (minHeap)
         minHeap = new PriorityQueue<Integer>();
         maxHeap = new PriorityQueue<Integer>((a,b) -> Integer.compare(b, a));
     }
     
     public void addNum(int num) {
+        // By default offering to maxHeap
         maxHeap.offer(num);
+        
+        // If size difference exceeds one OR if maxHeap has element greater than minHeaps smallest 
+        // we pop/poll form maxHeap and offer to minHeap
         if (
             maxHeap.size() - minHeap.size() > 1 ||
             !minHeap.isEmpty() && minHeap.peek() < maxHeap.peek()
         )
             minHeap.offer(maxHeap.poll());
         
+        // In case above operations results to a situation where difference exceeds one
+        // we this time offer minHeaps max to max heap.
         if (minHeap.size() - maxHeap.size() > 1)
             maxHeap.offer(minHeap.poll());
     }
