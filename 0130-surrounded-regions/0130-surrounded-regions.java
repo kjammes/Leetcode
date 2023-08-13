6579b976e074ade5b7fd8344a9e18420a355bb34
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-13 20:17:35 +0530

    Time: 2 ms (56.03%), Space: 43.8 MB (88.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,4 +1,9 @@
 class Solution {
+    
+    int[][] dirs = {
+        {0, 1}, {0, -1}, {1, 0}, {-1, 0}  
+    };
+    
     public void solve(char[][] board) {
         int m = board.length, n = board[0].length;
         
@@ -6,33 +11,18 @@ class Solution {
         
         for (int row = 0; row < m; row++) {
             if (board[row][0] == 'O') 
-                q.offer(new int[] {row, 0});
+                dfs(row, 0, board);
             if (board[row][n - 1] == 'O')
-                q.offer(new int[] {row, n-1});
+                dfs(row, n-1, board);
         }
         
         for (int col = 1; col < n - 1; col++) {
             if (board[0][col] == 'O')
-                q.offer(new int[] {0, col});
+                dfs(0, col, board);
             if (board[m - 1][col] == 'O')
-                q.offer(new int[] {m - 1, col});
+                dfs(m - 1, col, board);
         }
-        
-        int[][] dirs = {
-            {0, 1}, {0, -1}, {1, 0}, {-1, 0}  
-        };
-        while (!q.isEmpty()) {
-            int r = q.peek()[0], c = q.poll()[1];
-            board[r][c] = 'Z';
-            
-            for (int dir[]: dirs) {
-                int x = r + dir[0], y = c + dir[1];
-                if (x < 0 || y < 0 || x >= m || y >= n)
-                    continue;
-                if (board[x][y] == 'O')
-                    q.offer(new int[] {x, y});
-            }
-        }
+
         
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
@@ -41,6 +31,17 @@ class Solution {
                 else if (board[row][col] == 'O')
                     board[row][col] = 'X';
             }
+        }
+    }
+    
+    private void dfs(int r, int c, char[][] board) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != 'O')
+            return;
+        
+        board[r][c] = 'Z';
+        for (int[] dir: dirs) {
+            int x = r + dir[0], y = c + dir[1];
+            dfs(x, y, board);
         }
     }
 }
