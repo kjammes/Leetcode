ccdc5a6c59da1bf08324be80b2a884345d3be512
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-13 20:07:20 +0530

    Time: 5 ms (13.93%), Space: 43.8 MB (80.97%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,44 +1,46 @@
 class Solution {
-    
-    int rows, cols;
-    int[][] dirs = {
-        {0, 1},
-        {1, 0},
-        {0, -1},
-        {-1, 0}
-    };
-    
     public void solve(char[][] board) {
-        rows = board.length; cols = board[0].length;
+        int m = board.length, n = board[0].length;
         
-        for (int r = 0; r < rows; r++) {
-            if (board[r][0] == 'O') dfs(board, r, 0);
-            if (board[r][cols - 1] == 'O') dfs(board, r, cols - 1);
+        Queue<int[]> q = new LinkedList<>();
+        
+        for (int row = 0; row < m; row++) {
+            if (board[row][0] == 'O') 
+                q.offer(new int[] {row, 0});
+            if (board[row][n - 1] == 'O')
+                q.offer(new int[] {row, n-1});
         }
         
-        for (int c = 0; c < cols; c++) {
-            if (board[0][c] == 'O') dfs(board, 0, c);
-            if (board[rows - 1][c] == 'O') dfs(board, rows - 1, c);
+        for (int col = 1; col < n - 1; col++) {
+            if (board[0][col] == 'O')
+                q.offer(new int[] {0, col});
+            if (board[m - 1][col] == 'O')
+                q.offer(new int[] {m - 1, col});
         }
         
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (board[r][c] == 'O') board[r][c] = 'X';
-                if (board[r][c] == 'T') board[r][c] = 'O';  
+        int[][] dirs = {
+            {0, 1}, {0, -1}, {1, 0}, {-1, 0}  
+        };
+        while (!q.isEmpty()) {
+            int r = q.peek()[0], c = q.poll()[1];
+            board[r][c] = 'Z';
+            
+            for (int dir[]: dirs) {
+                int x = r + dir[0], y = c + dir[1];
+                if (x < 0 || y < 0 || x >= m || y >= n)
+                    continue;
+                if (board[x][y] == 'O')
+                    q.offer(new int[] {x, y});
             }
         }
-    }
-    
-    private void dfs(char[][] board, int r, int c) {
-        if (
-            r < 0 || c < 0 ||
-            r >= rows || c >= cols || board[r][c] != 'O'
-        )
-            return;
         
-        board[r][c] = 'T';
-        for (int[] dir: dirs) {
-            dfs(board, r + dir[0], c + dir[1]);
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == 'Z')
+                    board[row][col] = 'O';
+                else if (board[row][col] == 'O')
+                    board[row][col] = 'X';
+            }
         }
     }
 }
