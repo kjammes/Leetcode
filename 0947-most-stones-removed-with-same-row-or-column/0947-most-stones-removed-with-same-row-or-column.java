1c7c0dc6d463e61c1eb6a923d1df0d7f925fdbcc
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-20 19:10:16 +0530

    Time: 8 ms (93.64%), Space: 43.8 MB (80.04%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,53 @@
+class Solution {
+    Map<Integer, Integer> f = new HashMap<>();
+    int islands = 0;
+
+    public int removeStones(int[][] stones) {
+        for (int i = 0; i < stones.length; ++i)
+            union(stones[i][0], ~stones[i][1]);
+        return stones.length - islands;
+    }
+
+    public int find(int x) {
+        if (f.putIfAbsent(x, x) == null)
+            islands++;
+        if (x != f.get(x))
+            f.put(x, find(f.get(x)));
+        return f.get(x);
+    }
+
+    public void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            f.put(x, y);
+            islands--;
+        }
+    }
+}   
+
+class DSU {
+    int[] parent;
+    
+    DSU(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+        }
+    }
+}
