6c924d2f360d38a96d615317a2af6b6d25cd90fa
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-20 19:31:20 +0530

    Time: 14 ms (77.79%), Space: 43.2 MB (99.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,35 +1,69 @@
 class Solution {
     public int removeStones(int[][] stones) {
-        DSU dsu = new DSU();
-        
-        for (int[] stone : stones) {
-            dsu.union(stone[0], ~stone[1]);
+        int n = stones.length;
+
+        int maxRow = 0;
+        int maxCol = 0;
+        for (int i = 0; i < n; i++) {
+            maxRow = Math.max(maxRow, stones[i][0]);
+            maxCol = Math.max(maxCol, stones[i][1]);
         }
-        
-        return stones.length - dsu.islands;
+
+        DisjointSet ds = new DisjointSet(20001);
+        HashMap<Integer, Integer> stoneNodes = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int nodeRow = stones[i][0];
+            int nodeCol = stones[i][1] + maxRow + 1;
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes.put(nodeRow, 1);
+            stoneNodes.put(nodeCol, 1);
+        }
+
+        int cnt = 0;
+        for (Map.Entry<Integer, Integer> entry : stoneNodes.entrySet()) {
+            if (ds.findUPar(entry.getKey()) == entry.getKey()) {
+                cnt++;
+            }
+        }
+
+        return n - cnt;
     }
 }
 
-class DSU {
-    Map<Integer, Integer> f = new HashMap<>();
-    int islands = 0;
-    
-    public int find(int x) {
-        if (f.putIfAbsent(x, x) == null) {
-            islands++;
+class DisjointSet {
+    int[] parent;
+    int[] size;
+
+    public DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
-        if (x != f.get(x)) {
-            f.put(x, find(f.get(x)));
-        }
-        return f.get(x);
     }
 
-    public void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) {
-            f.put(x, y);
-            islands--;
+    public int findUPar(int u) {
+        if (parent[u] == u) {
+            return u;
+        }
+        return parent[u] = findUPar(parent[u]);
+    }
+
+    public void unionBySize(int u, int v) {
+        int rootU = findUPar(u);
+        int rootV = findUPar(v);
+
+        if (rootU != rootV) {
+            if (size[rootU] > size[rootV]) {
+                parent[rootV] = rootU;
+                size[rootU] += size[rootV];
+            } else {
+                parent[rootU] = rootV;
+                size[rootV] += size[rootU];
+            }
         }
     }
 }
