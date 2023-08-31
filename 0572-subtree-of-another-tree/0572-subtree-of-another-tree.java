8f8aeef65cf24bcb25b0dc431061660425fa6c32
Jayesh Karli <44406332+kjammes@users.noreply.github.com>
2023-08-31 21:03:23 +0530

    Time: 2 ms (97.77%), Space: 43.4 MB (31.13%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,17 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null || isSameTree(root, subRoot))
+        if (subRoot == null || isSubtreeH(root, subRoot))
             return true;
         if (root == null)
             return false;
+        
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-
-    private boolean isSameTree(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null)
+    
+    private boolean isSubtreeH(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null)
             return true;
-        if (root1 == null || root2 == null || root1.val != root2.val)
+        if (root == null || subRoot == null || root.val != subRoot.val)
             return false;
-        return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+        
+        return isSubtreeH(root.left, subRoot.left) && isSubtreeH(root.right, subRoot.right);
     }
 }
+
+
